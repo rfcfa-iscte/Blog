bec286cacc42db164f12b8cd785f01dce3a80610
rfcfa-iscte <78759743+rfcfa-iscte@users.noreply.github.com>
2024-03-30 19:14:30 +0000

    techstack fixed icons for sql etc..

--- src/components/about/Technology.jsx
+++ src/components/about/Technology.jsx
@@ -8,21 +8,27 @@ import {
   faPhp,
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { SiMysql } from "react-icons/si";
+import { DiMongodb } from "react-icons/di";
+import { FaReact } from "react-icons/fa";
 
 export const Technology = () => {
   return (
     <div className="programming-languages-section">
       <div className="icon-container">
         <FontAwesomeIcon icon={faJava} className="programming-language-icon" />
+        <FaReact size={50} className="programming-language-icon" />
         <FontAwesomeIcon icon={faHtml5} className="programming-language-icon" />
         <FontAwesomeIcon icon={faCss3} className="programming-language-icon" />
         <FontAwesomeIcon icon={faJs} className="programming-language-icon" />
         <FontAwesomeIcon icon={faPhp} className="programming-language-icon" />
-        <img
+        <SiMysql size={50} className="programming-language-icon" />
+        <DiMongodb size={50} className="programming-language-icon" />
+        {/* <img
           src="../images/code/mysql-logo-svgrepo-com.svg"
           alt="MySQL"
           className="programming-language-icon"
-        />
+        /> */}
         {/* Add more icons as needed */}
       </div>
     </div>
