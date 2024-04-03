96b75995305bb0bfeea5b1f9f3a4c4ad28087fae
rfcfa-iscte <78759743+rfcfa-iscte@users.noreply.github.com>
2024-04-03 20:39:31 +0100

    Home title effect

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import "./home.css";
+import { TypeAnimation } from "react-type-animation";
 
 export const Home = () => {
+
   return (
     <>
-      <h1 className="title_1">Welcome!</h1>
-      <div className="typewriter">
-        <h2>My name is Rui Ferreira</h2>
-      </div>
-      <h3></h3>
-      <div className="profile-image">
-        <img src="../images/about/pfp.png" alt="Your Image" />
+      <div className="landing_page">
+        <div className="left_text">
+          <h1 className="title_1">Welcome!</h1>
+          <TypeAnimation
+            className="identification"
+            sequence={["I'm Rui", 1000, "I'm a Software Developer", 1000]}
+            wrapper="span"
+            speed={50}
+            style={{ fontSize: "3em", display: "inline-block", color: "#fec544" }}
+            repeat={Infinity}
+          />
+        </div>
+        <div className="profile_image">
+          <img src="../images/about/pfp.png" alt="Your Image" />
+        </div>
       </div>
     </>
   );
