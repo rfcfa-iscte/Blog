64cc25cf7edfbe7a1ed35b8b3f48bd16d2e7e653
rfcfa-iscte <78759743+rfcfa-iscte@users.noreply.github.com>
2024-04-08 03:20:36 +0100

    email tooltip for success and insucess of sending email

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -1,20 +1,20 @@
 import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import "./contact.css";
+import Tooltip from "@mui/material/Tooltip";
+import Fade from "@mui/material/Fade";
 
 export const Form = () => {
   const [isClicked, setIsClicked] = useState(false);
-  // const [showTooltip, setShowTooltip] = useState(false);
-  
+  const [showTooltip, setShowTooltip] = useState(false);
+  const [tooltipContent, setTooltipContent] = useState('');
+
   const handleMouseDown = () => {
     setIsClicked(true);
   };
 
   const handleMouseUp = () => {
     setIsClicked(false);
-  };
-  const handleClick = () => {
-    return <div className="tooltip">EMAIL SENT</div>;
   };
   const form = useRef();
 
@@ -31,32 +31,70 @@ export const Form = () => {
       .then(
         (result) => {
           console.log(result.text);
+          setTooltipContent('Email Sent');
+          setShowTooltip(true);
+          setTimeout(() => {
+            setShowTooltip((prevState) => !prevState);
+          }, 3000);
+          
         },
         (error) => {
           console.log(error.text);
+          setTooltipContent('Try Again Later');
+          setShowTooltip(true);
+          setTimeout(() => {
+            setShowTooltip((prevState) => !prevState);
+          }, 3000);
+          
         }
       );
   };
 
   return (
-    <div className="title">
-      <form ref={form} onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Message</label>
-        <textarea name="message" />
-        <input
-          type="submit"
-          value="Send"
-          className={isClicked ? "clicked" : "f-butt"}
-          onMouseDown={handleMouseDown}
-          onClick={handleClick}
-          onMouseUp={handleMouseUp}
-        />
-        {/* {showTooltip ? <div className="tooltip">Email sent</div> : ""} */}
-      </form>
-    </div>
+    <>
+      {showTooltip ? (
+        <div className="title">
+          <form ref={form} onSubmit={sendEmail}>
+            <label>Name</label>
+            <input type="text" name="user_name" />
+            <label>Email</label>
+            <input type="email" name="user_email" />
+            <label>Message</label>
+            <textarea name="message" />
+            <Tooltip
+              TransitionComponent={Fade}
+              TransitionProps={{ timeout: 600 }}
+              title={tooltipContent}
+            >
+              <input
+                type="submit"
+                value="Send"
+                className={isClicked ? "clicked" : "f-butt"}
+                onMouseDown={handleMouseDown}
+                onMouseUp={handleMouseUp}
+              />
+            </Tooltip>
+          </form>
+        </div>
+      ) : (
+        <div className="title">
+          <form ref={form} onSubmit={sendEmail}>
+            <label>Name</label>
+            <input type="text" name="user_name" />
+            <label>Email</label>
+            <input type="email" name="user_email" />
+            <label>Message</label>
+            <textarea name="message" />
+            <input
+              type="submit"
+              value="Send"
+              className={isClicked ? "clicked" : "f-butt"}
+              onMouseDown={handleMouseDown}
+              onMouseUp={handleMouseUp}
+            />
+          </form>
+        </div>
+      )}
+    </>
   );
 };
